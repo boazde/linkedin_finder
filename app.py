0bf86b79c757989901dc758357e4e6cef9fe66d9
Boaz Descalo <boaz@descalo.com>
2021-05-18 23:59:00 +0300

    update download template

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ import pandas as pd
 import base64
 from src.scrap.scrap import get_df_url
 
-def download_csv(df,fname):
+def download_csv(df,fname,button_name):
      csv = df.to_csv(index=False)
      b64 = base64.b64encode(csv.encode()).decode() 
-     href = f'<a href="data:file/csv;base64,{b64}" download="{fname}">Download csv</a>'
+     href = f'<a href="data:file/csv;base64,{b64}" download="{fname}">{button_name}</a>'
      return href
 
 
@@ -17,6 +17,7 @@ def main():
 
 
     uploaded_file = st.file_uploader("Upload a spreadsheet file of First Name; Last Name ;Company")
+    st.markdown(download_csv(pd.DataFrame([], columns = ['First Name', 'Last Name','Company']),'linkedin_finder.csv',"Download Template"), unsafe_allow_html=True)
     if uploaded_file is not None:
         df = pd.read_csv(uploaded_file)
         st.write(df)
@@ -26,7 +27,7 @@ def main():
             st.write('Working on the first '+ str(max_leads) +' leads')
             df = get_df_url(df,max_leads)
             st.write(df)
-            st.markdown(download_csv(df,'linkedin_finder.csv'), unsafe_allow_html=True)
+            st.markdown(download_csv(df,'linkedin_finder.csv',"Download csv"), unsafe_allow_html=True)
         if st.button('Get leads (Premium)'):
             max_leads = 100
             email = st.text_input('Email Address')
